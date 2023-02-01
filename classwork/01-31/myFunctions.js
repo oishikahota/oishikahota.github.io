6cf2929fb3de184423abedae70624fad15f8467c
oishikahota <oishikahota0108@gmail.com>
2023-01-31 19:28:44 -0500

    wa

--- classwork/01-31/myFunctions.js
+++ classwork/01-31/myFunctions.js
@@ -1,25 +1,3 @@
 <script>
-function returnsSq(val) {
-    if (isNaN(val)) {
-        console.log("Not a number.")
-    } else {
-        return Math.sqrt(val);
-    }
-
-    function decreasing(val) {
-        if (isNaN(val)) {
-            console.log("Not a number.");
-        } else {
-            // There are a few different ways to implement this.
-            // The following is a simple implementation with a for loop
-
-            for (let i=val; i >= 0; i--) {
-                console.log(i);
-            }
-        }
-
-        function sum(a, b) {
-            c = a + b;
-            return c;
-          }
+<link = "https://oishikahota.github.io/classwork/01-31/style.css"/>
  </script>
